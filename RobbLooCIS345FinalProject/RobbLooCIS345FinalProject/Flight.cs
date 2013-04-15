2c04d988aa34888a19132312945d75d40d3bcf3c
ruby -v <nathandrobb@gmail.com>
2013-04-15 12:22:10 -0700

    added method to read flight info(menu item #2)...
    also wrote Flight() method that takes flight info as parameters and
    get/set properties for the flight info

--- RobbLooCIS345FinalProject/RobbLooCIS345FinalProject/Flight.cs
+++ RobbLooCIS345FinalProject/RobbLooCIS345FinalProject/Flight.cs
@@ -8,6 +8,10 @@ namespace RobbLooCIS345FinalProject
 {
     class Flight
     {
+        public int FlightNumber { get; set; }
+        public string OriginLocation { get; set; }
+        public string DestinationLocation { get; set; }
+
         //delcare private 2d array. 
         private string [,] FlightArray = new string[20,3];//prototype tracks 20 different flights with flight number, origin, and destination
 
@@ -15,6 +19,17 @@ namespace RobbLooCIS345FinalProject
         public Flight ()
         {
             
+        }
+
+        public Flight(int number, string startLocation, string endLocation)
+        {
+            FlightNumber = number;
+            OriginLocation = startLocation;
+            DestinationLocation = endLocation;
+            //playing with how to fill array with new flight info, struggling with this.
+            FlightArray[0, 0] = Convert.ToString(number);
+            startLocation = FlightArray[0, 1];
+            endLocation = FlightArray[0, 2];
         }
 
         //get/set assessor for FlightNumber using the ArrayIndex Parameter 
--- RobbLooCIS345FinalProject/RobbLooCIS345FinalProject/NeatsMainMenu.cs
+++ RobbLooCIS345FinalProject/RobbLooCIS345FinalProject/NeatsMainMenu.cs
@@ -8,6 +8,8 @@ namespace RobbLooCIS345FinalProject
 {
     class NeatsMainMenu
     {
+        private Flight flight;
+        string[,] FlightArray;
         //declare submenu
         NeatsSubMenu SubMenu; 
 
@@ -45,7 +47,7 @@ namespace RobbLooCIS345FinalProject
         private void DisplayMenu()
         {   
             //the 3 lines resize the console window size (not really important)
-            Console.SetWindowSize(100, 40);
+            Console.SetWindowSize(80, 25);
             Console.BufferHeight = 100;
             Console.BufferWidth = 100;
 
@@ -76,11 +78,14 @@ namespace RobbLooCIS345FinalProject
                 case 2:
                     //add a new flight
                     //first have initialize an instance of the class (create a new object of the class)
-                    SubMenu = new NeatsSubMenu();
-                    SubMenu.DisplaySubMenu();
+                    ReadFlight();
                     break;
                 case 3:
                     //select a flight
+                    //moved submenu lines below to case 3 as that's how its implemented in the NEAT.exe example he gave us
+                    SubMenu = new NeatsSubMenu();
+                    SubMenu.DisplaySubMenu();
+                    
                     break;
                 case 4:
                     //search by passenger
@@ -95,18 +100,17 @@ namespace RobbLooCIS345FinalProject
         }
 
 
-            //Method for displaying default flight info initially before array is populated by our data
+        //Method for displaying default flight info initially before array is populated by our data
         //There is probably a better way to do this, just messing around with arrays at this point.
         //pressing any key takes us back to main menu
         public void DummyFlightInfo()
         {
             Console.Clear();
-
-            string[,] FlightArray = { {"700","Seattle","Phoenix"}, 
+            string[,] FlightArray = { {"700","Seattle","Phoenix"},
                                        {"600","Chicago","New York"},
                                        {"500","Phoenix","San Diego"}, 
                                        {"656","Denver","Austin"}};
-
+            
             Console.WriteLine("\t\t\tCurrent Flights - NEATS system\n\n");
             Console.WriteLine("\t1. #{0}\t\t{1}\t-->\t{2}", FlightArray[0, 0], FlightArray[0, 1], FlightArray[0, 2]);
             Console.WriteLine("\t2. #{0}\t\t{1}\t-->\t{2}", FlightArray[1, 0], FlightArray[1, 1], FlightArray[1, 2]);
@@ -115,13 +119,31 @@ namespace RobbLooCIS345FinalProject
 
             Utilities.Back();
             Console.Clear();
-            DisplayMenu();
-            
-            
-           
-        
-                    
+            DisplayMenu();          
+        }
 
+        //method to read flight info (menu item #2)
+        //trying to figure out best way to add flight info to FlightArray.
+
+        public void ReadFlight()
+        {
+            int number = 0;
+            string startLocation = "";
+            string endLocation = "";
+            Console.Clear();
+            Console.WriteLine("\t\tEnter New Flight - NEATS System\n\n");
+            Console.Write("Enter flight number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Origin: ");
+            startLocation = Console.ReadLine();
+            Console.Write("Enter Destination: ");
+            endLocation = Console.ReadLine();
+
+            flight = new Flight(number, startLocation, endLocation);
+            
+            
+            Console.Clear();
+            DisplayMenu();
         }
         
     }
